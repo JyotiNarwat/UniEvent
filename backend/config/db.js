3a29764a2c48a7aa05b113ede91d0cfff0c3d5f2
JyotiNarwat <jyotinarwat390@gmail.com>
2026-03-13 12:39:22 +0530

    final commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,3 @@
-// import mongoose from "mongoose";
-
-// export const connectDb = async()=>{
-//     await mongoose.connect('mongodb+srv://jyotinarwat390_db_user:eQOmuTzEAk1leZKO@cluster0.8sxk1tm.mongodb.net/UniEvent').then(()=>{console.log("DB connected")});
-// }
 
 import mongoose from "mongoose";
 import dotenv from "dotenv";
