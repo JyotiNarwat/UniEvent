4ee2e468b3f6c7dbd220a54e966c9c2f7d77f82c
JyotiNarwat <jyotinarwat390@gmail.com>
2026-03-13 12:31:26 +0530

    Added DB connection with env variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,19 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-export const connectDb = async()=>{
-    await mongoose.connect('mongodb+srv://jyotinarwat390_db_user:eQOmuTzEAk1leZKO@cluster0.8sxk1tm.mongodb.net/UniEvent').then(()=>{console.log("DB connected")});
-}
+// export const connectDb = async()=>{
+//     await mongoose.connect('mongodb+srv://jyotinarwat390_db_user:eQOmuTzEAk1leZKO@cluster0.8sxk1tm.mongodb.net/UniEvent').then(()=>{console.log("DB connected")});
+// }
+
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB connected");
+  } catch (error) {
+    console.error("DB connection failed:", error.message);
+  }
+};
